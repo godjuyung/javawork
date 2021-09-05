7a7295fed8efee8e34a642ad3b4d11b57c72a124
grey0124 <grey0124@naver.com>
2021-09-05 11:37:22 +0900

    Add 파일 추가

--- day05/src/arraysample/Book.java
+++ day05/src/arraysample/Book.java
@@ -4,6 +4,8 @@ public class Book {
 	private String bookName;
 	private String author;
 	
+	public Book() {}  //사용하는 쪽에서 호출하면 명시해야 한다
+	
 	public Book(String bookName, String author) {
 		this.bookName = bookName;
 		this.author = author;
@@ -17,7 +19,14 @@ public class Book {
 		this.bookName = bookName;
 	}
 	
+	public String getBookName() {
+		return bookName;
+	}
+	
 	public void setAuthor(String author) {
 		this.author = author;
+	}
+	public String getauthor() {
+		return author;
 	}
 }
